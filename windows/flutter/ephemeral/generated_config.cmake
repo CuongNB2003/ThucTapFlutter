8fd2b4e9204886724c8944bff213c1382ea8b51a
cuongnbph22662 <bacuong15423@gmail.com>
2023-10-19 19:04:41 +0700

    chat_app_v1

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\FLutterExample\\thuc_tap_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=E:\\FLutterExample\\thuc_tap_flutter"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\FLutterExample\\thuc_tap_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\FLutterExample\\thuc_tap_flutter"
+  "FLUTTER_TARGET=E:\\FLutterExample\\thuc_tap_flutter\\lib\\views\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85MDY0NDU5YThiMGRjZDMyODc3MTA3ZjYwMDJjYzQyOWE3MTY1OWQxLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\FLutterExample\\thuc_tap_flutter\\.dart_tool\\package_config.json"
+)
